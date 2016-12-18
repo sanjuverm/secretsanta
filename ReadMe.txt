d31651dc4c1ad57eacded830eccd4ed540f663fb
Sanjna Verma <sanj@nyu.edu>
2016-12-17 20:31:25 -0500

    adding models and a readme

---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+This application is a SecretSanta game generator. 
+
+It is made in Django/Python, SQLite3, and will be updated as time goes on
+
+This first version is just to get functionality of the site
+V2: make it look good
+V3: incorporate shopping APIs
+V4: figure out how to get advertisements on the site
+
+Thanks for taking a look at this pet project!
+
--- polls/models.py
+++ polls/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200) #what would you like for Christmas
+    pub_date = models.DateTimeField('date published') #answer response time
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) #what would you like for christmas
+    choice_text = models.CharField(max_length=200) #dogs, cats, bears
+    votes = models.IntegerField(default=0) #up vote/ down vote (up vote by 1 if gift purchased) 
